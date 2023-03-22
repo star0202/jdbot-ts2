560bf9f65f886272e78d5b2f51dab239ceab0519
Starcea <stardev.uwu@gmail.com>
2023-03-22 17:47:45 +0900

    feat: colored log

--- src/modules/events/default.ts
+++ src/modules/events/default.ts
@@ -1,9 +1,10 @@
 import { Extension, listener } from '@pikokr/command.ts'
+import { green } from 'chalk'
 
 class DefaultEvents extends Extension {
   @listener({ event: 'ready' })
   async ready() {
-    this.logger.info(`Logged in as ${this.client.user?.tag}`)
+    this.logger.info(`Logged in as: ${green(this.client.user?.tag)}`)
     await this.commandClient.fetchOwners()
   }
 }
