e8549298952a12e860e6264645809106698322a6
Starcea <stardev.uwu@gmail.com>
2023-03-24 16:26:38 +0900

    fix: typing

--- src/utils/message.ts
+++ src/utils/message.ts
@@ -1,5 +1,5 @@
 import { Message } from 'discord.js'
 
-export const isMessageInvalid = (msg: Message) => {
+export const isMessageInvalid = (msg: Message): boolean => {
   return msg.author.bot || msg.guild === null || msg.content.length === 0
 }
